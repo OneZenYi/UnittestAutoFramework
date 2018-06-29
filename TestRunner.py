cba3d060d6fc6b381dd46a43fa5330c2fc2d322b
zhangyi2 <zhangyi@faxindai.com>
2018-06-29 15:28:42 +0800

    1、新增基础框架目录 2、新增邮箱、钉钉发送信息功能 3、新增测试报告文件 4、封装Get、Post、Put 请求方式类

--- TestRunner.py
+++ TestRunner.py
@@ -5,8 +5,20 @@
 import sys
 import time
 import unittest
-
 from PublicPackage import HTMLTestRunnerCN, Email,Dingtalk
+
+# 导入用例文件；导入数据文件;导入控件文件
+sys.path.append('./Interface')
+sys.path.append('./TestData')
+sys.path.append('./TestCase')
+sys.path.append('./PublicPackage')
+
+# 指定测试用例为当前文件夹下的 interface 目录
+test_dir = './TestCase'
+
+# 执行所有用例将 pattern 参数改成 *
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_run.py')
+
 
 if __name__ == "__main__":
 
@@ -15,14 +27,11 @@ if __name__ == "__main__":
   desc = '右侧饼状图展示通过率、失败率、错误率'
 
   now = time.strftime("%Y-%m-%d %H_%M_%S", time.localtime())
-  report_file_now = 'report/TestReport'+now+'.html'
+  report_file_now = 'TestReport/TestReport'+now+'.html'
 
   report_file = '/TestReport/TestReport.html'
-  # 创建报告文件，打印报告数据
 
-  '''with open(report_file_0, 'wb') as report:
-    runner = HTMLTestRunnerCN.HTMLTestRunner(stream=report, title=report_title, description=desc)
-    runner.run(discover1)'''
+  # 创建报告文件，打印报告数据
 
   with open(report_file, 'wb') as report:
       runner = HTMLTestRunnerCN.HTMLTestRunner(stream=report, title=report_title, description=desc)
